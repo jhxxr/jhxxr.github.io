33806b63582a049750e53c130e1107581d71680c
jhxxr <2471717907@qq.com>
2022-05-12 16:31:49 +0800

    Site updated: 2022-05-12 16:31:47

---
+++ js/randombg.js
@@ -0,0 +1,31 @@
+//随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
+var backimg =[
+    "url(https://npm.elemecdn.com/jhxxr-cdn/tu/b1.jpg)",
+    "url(https://npm.elemecdn.com/jhxxr-cdn/tu/b2.jpg)",
+    "url(https://npm.elemecdn.com/jhxxr-cdn/tu/b3.jpg)",
+    "url(https://npm.elemecdn.com/jhxxr-cdn/tu/b4.jpg)",
+    "url(https://npm.elemecdn.com/jhxxr-cdn/tu/b5.jpg)",
+    "url(https://npm.elemecdn.com/jhxxr-cdn/tu/b6.jpg)"
+  ];
+  //获取背景图片总数，生成随机数
+  var bgindex =Math.floor(Math.random() * backimg.length);
+  //重设背景图片
+  document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
+  //我坚持一图流！
+  document.getElementById("page-header").style.backgroundImage = backimg[bgindex];
+  //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
+  /*
+  var bannerimg =[
+    "url(https://npm.elemecdn.com/jhxxr-cdn/tu/b1.jpg)",
+    "url(https://npm.elemecdn.com/jhxxr-cdn/tu/b2.jpg)",
+    "url(https://npm.elemecdn.com/jhxxr-cdn/tu/b3.jpg)",
+    "url(https://npm.elemecdn.com/jhxxr-cdn/tu/b4.jpg)",
+    "url(https://npm.elemecdn.com/jhxxr-cdn/tu/b5.jpg)",
+    "url(https://npm.elemecdn.com/jhxxr-cdn/tu/b6.jpg)"
+  ];
+  //获取banner图片总数，生成随机数
+  var bannerindex =Math.floor(Math.random() * bannerimg.length);
+  //重设banner图片
+  document.getElementById("page-header").style.backgroundImage = bannerimg[bannerindex];
+
+ */
