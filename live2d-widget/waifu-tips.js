92aa39885a51faac5c2de9dfda147fb9d3d16774
jhxxr <2471717907@qq.com>
2022-05-17 11:40:12 +0800

    Site updated: 2022-05-17 11:40:11

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -65,7 +65,7 @@ function loadWidget(config) {
 				window.ASTEROIDSPLAYERS.push(new Asteroids());
 			} else {
 				const script = document.createElement("script");
-				script.src = "https://cdn.jsdelivr.net/gh/stevenjoezhang/asteroids/asteroids.js";
+				script.src = "https://cdn1.tianli0.top/gh/stevenjoezhang/asteroids/asteroids.js";
 				document.head.appendChild(script);
 			}
 		});
